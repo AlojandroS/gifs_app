05de6e11cb5fea98b76bded338f45f23258817db
Alojandro <aloserra92@gmail.com>
2024-02-09 10:48:55 +0100

    project

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LazyImageComponent } from '../gifs/components/lazy-image/lazy-image.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    LazyImageComponent,
+    SidebarComponent
+       
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LazyImageComponent,
+    SidebarComponent
+    
+  ]
+})
+export class SharedModule { }
